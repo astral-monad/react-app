af580f568e159053c86c8c38ad56f8e442264854
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2021-09-07 14:47:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation } from "./src/components/navigation";
-import { Footer } from "./src/components/footer";
-import { Home } from "./src/components/home";
-import { About } from "./src/components/about";
-import { Buy } from "./src/components/buy";
-import { Contact } from "./src/components/contact";
+import Navigation from "./components/Navigation";
+import Footer  from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Buy from "./components/Buy";
+import Contact from "./components/Contact";
 
 function App() {
   return (
