aa79fc2ca13f6526a9fad88ddbae7d688424863c
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2021-09-07 14:11:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, About, Buy, Contact } from "./components";
+import { Navigation, Footer, Home, About, Buy, Contact } from "/components";
 function App() {
   return (
     <div className="App">
